ca54ca8ad6f2d4007aaa5899543df9dbe57aa0f6
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-08-16 14:13:49 +0200

    add banner
    with Keyframes

---
+++ components/parts/Add.tsx
@@ -0,0 +1,15 @@
+const Add = () => {
+    return (
+      <div className="relative bg-gray-100 text-center overflow-hidden">
+          <div className="whitespace-nowrap animate-marquee">
+            <span className="pr-8">
+              Акція! З 15 по 24 липня на групи комодів, тумб для взуття та
+              передпокої!
+            </span>
+          </div>
+        </div>
+    );
+  };
+  
+  export default Add;
+  
--- components/sections/Layout.tsx
+++ components/sections/Layout.tsx
@@ -1,14 +1,16 @@
-import { TLayout } from '@/types'
-import { FC } from 'react'
-import { Footer } from './Footer'
-import { Header } from './Header/Header'
+import { TLayout } from "@/types";
+import { FC } from "react";
+import { Footer } from "./Footer";
+import { Header } from "./Header/Header";
+import Add from "../parts/Add";
 
 export const Layout: FC<TLayout> = ({ children }) => {
-	return (
-		<>
-			<Header />
-			<main className='container'>{children}</main>
-			<Footer />
-		</>
-	)
-}
+  return (
+    <>
+      <Add />
+      <Header />
+      <main className="container">{children}</main>
+      <Footer />
+    </>
+  );
+};
