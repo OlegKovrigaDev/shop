c1be739d3ed04d84852495e443aa770bb7b7c0a7
olegkovrigadev <olegkovrigadev@gmail.com>
2024-08-20 13:32:14 +0300

    Update Product Card and adaptive

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -1,35 +1,36 @@
-import { fetchCategoryId, fetchCategoryItems } from "@/api/index";
-import { CategoryPageProps, TItems } from "@/types";
-import { ProductCard } from "@/components/parts/ProductCard";
-import { CrumbsLink } from "@/components/parts/CrumbsLink";
+import { fetchCategoryId, fetchCategoryItems } from '@/api/index'
+import { CrumbsLink } from '@/components/parts/CrumbsLink'
+import { ProductCard } from '@/components/parts/ProductCard'
+import { CategoryPageProps, TItems } from '@/types'
 
 const CategoryPage = async ({ params }: CategoryPageProps) => {
-  const { id } = params;
-  let items: TItems[] = [];
+	const { id } = params
+	let items: TItems[] = []
 
-  let categories = await fetchCategoryId(id);
-  items = await fetchCategoryItems(id);
+	let categories = await fetchCategoryId(id)
+	items = await fetchCategoryItems(id)
 
-  return (
-    <div className="container">
-      <CrumbsLink categories={categories} items={items} />
-      <div className="flex flex-1 flex-wrap gap-3 md:basis-1/2 lg:basis-1/3">
-        {items.map(
-          ({
-            offerId,
-            params: { GoodName, RetailPrice, RetailPriceWithDiscount },
-          }) => (
-            <ProductCard
-              key={offerId}
-              title={GoodName}
-              oldPrice={RetailPriceWithDiscount}
-              newPrice={RetailPrice}
-            />
-          )
-        )}
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='container'>
+			<CrumbsLink categories={categories} items={items} />
+			<div className='flex flex-1 flex-wrap gap-3 md:basis-1/2 lg:basis-1/3 max-w-[904px]'>
+				{items.map(
+					({
+						offerId,
+						params: { GoodName, RetailPrice, RetailPriceWithDiscount },
+					}) => (
+						<ProductCard
+							key={offerId}
+							img='/slide1-min.jpg'
+							title={GoodName}
+							oldPrice={RetailPriceWithDiscount}
+							newPrice={RetailPrice}
+						/>
+					)
+				)}
+			</div>
+		</div>
+	)
+}
 
-export default CategoryPage;
+export default CategoryPage
