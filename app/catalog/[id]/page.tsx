957dd42c1ce266d1536c1b2f989511f4c9085208
olegkovrigadev <olegkovrigadev@gmail.com>
2024-08-20 15:43:01 +0300

    adaptive

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -12,10 +12,10 @@ const CategoryPage = async ({ params }: CategoryPageProps) => {
 	items = await fetchCategoryItems(id)
 
 	return (
-		<>
+		<div className='mb-[75px]'>
 			<CrumbsLink categories={categories} items={items} />
-			<div className='flex justify-between gap-8'>
-				<div className='flex flex-col gap-2 min-w-[280px]'>
+			<div className='flex flex-col gap-8 md:flex-row md:justify-between'>
+				<div className='flex flex-col gap-2 max-w-[280px] sm:min-w-[280px]'>
 					<FilterAccordion title='Filter' />
 					<FilterAccordion title='Filter' />
 					<FilterAccordion title='Filter' />
@@ -25,7 +25,7 @@ const CategoryPage = async ({ params }: CategoryPageProps) => {
 					<FilterAccordion title='Filter' />
 					<FilterAccordion title='Filter' />
 				</div>
-				<div className='flex flex-1 gap-y-8 justify-between flex-wrap md:basis-1/2 lg:basis-1/3 max-w-[904px]'>
+				<div className='flex flex-1 gap-y-8 justify-between flex-wrap max-w-[904px]'>
 					{items.map(
 						({
 							offerId,
@@ -42,7 +42,7 @@ const CategoryPage = async ({ params }: CategoryPageProps) => {
 					)}
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
