4fd59605b6cb800c61e43e1470fef2ddaf770808
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-08-25 14:06:21 +0200

    Update FilterAccordion.tsx

--- components/parts/FilterAccordion.tsx
+++ components/parts/FilterAccordion.tsx
@@ -1,23 +1,28 @@
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from '@/components/ui/accordion'
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { FC } from "react";
 
-export const FilterAccordion = ({ title }) => {
-	return (
-		<div className='bg-white w-full p-4 rounded-[4px]'>
-			<Accordion type='single' collapsible>
-				<AccordionItem value='item-1' className='border-transparent'>
-					<AccordionTrigger className='bg-[#C6C6C7] px-4 py-2'>
-						{title}
-					</AccordionTrigger>
-					<AccordionContent className='flex max-w-[248px] mt-6'>
-						Yes. It adheres to the WAI-ARIA design pattern.
-					</AccordionContent>
-				</AccordionItem>
-			</Accordion>
-		</div>
-	)
+interface FilterAccordionProps {
+  title: string;
 }
+
+export const FilterAccordion: FC<FilterAccordionProps> = ({ title }) => {
+  return (
+    <div className="bg-white w-full p-4 rounded-[4px]">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1" className="border-transparent">
+          <AccordionTrigger className="bg-[#C6C6C7] px-4 py-2">
+            {title}
+          </AccordionTrigger>
+          <AccordionContent className="flex max-w-[248px] mt-6">
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
