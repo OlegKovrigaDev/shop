77edd90a3782575826180def75ed00eba44310e4
olegkovrigadev <olegkovrigadev@gmail.com>
2024-08-28 21:45:00 +0300

    Add product page

--- components/parts/FilterAccordion.tsx
+++ components/parts/FilterAccordion.tsx
@@ -1,28 +1,33 @@
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { FC } from "react";
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '@/components/ui/accordion'
+import { FC, PropsWithChildren } from 'react'
 
-interface FilterAccordionProps {
-  title: string;
+interface FilterAccordionProps extends PropsWithChildren {
+	title: string
+	className?: string
 }
 
-export const FilterAccordion: FC<FilterAccordionProps> = ({ title }) => {
-  return (
-    <div className="bg-white w-full p-4 rounded-[4px]">
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="border-transparent">
-          <AccordionTrigger className="bg-[#C6C6C7] px-4 py-2">
-            {title}
-          </AccordionTrigger>
-          <AccordionContent className="flex max-w-[248px] mt-6">
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
-  );
-};
+export const FilterAccordion: FC<FilterAccordionProps> = ({
+	title,
+	className,
+	children,
+}) => {
+	return (
+		<div className='bg-white w-full p-4 rounded-[4px]'>
+			<Accordion type='single' collapsible>
+				<AccordionItem value='item-1' className='border-transparent'>
+					<AccordionTrigger className={`bg-[#C6C6C7] px-4 py-2 ${className}`}>
+						{title}
+					</AccordionTrigger>
+					<AccordionContent className='flex mt-6 pb-0'>
+						{children}
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
+		</div>
+	)
+}
--- components/sections/Layout.tsx
+++ components/sections/Layout.tsx
@@ -1,17 +1,16 @@
-import { TLayout } from "@/types";
-import { FC } from "react";
-import { Footer } from "./Footer";
-import { Header } from "./Header/Header";
-import { Ad } from "../parts/Ad";
+import { TLayout } from '@/types'
+import { FC } from 'react'
+import { Ad } from '../parts/Ad'
+import { Footer } from './Footer'
+import { Header } from './Header/Header'
 
 export const Layout: FC<TLayout> = ({ children }) => {
-  return (
-    <>
-      <Ad />
-      <Header />
-      <main className="container">{children}</main>
-      <Footer />
-      
-    </>
-  );
-};
+	return (
+		<>
+			<Ad />
+			<Header />
+			<main className='container'>{children}</main>
+			<Footer />
+		</>
+	)
+}
