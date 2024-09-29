bbcd742a0ae83bda3b35794531881ab64b7f28d5
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-09-29 11:14:17 +0200

    Update PageLayout.tsx

--- components/sections/PageLayout.tsx
+++ components/sections/PageLayout.tsx
@@ -1,33 +1,33 @@
-import { PageLayoutProps } from '@/types'
-import { CrumbsLink } from '../parts'
+import { PageLayoutProps } from "@/types";
+import { CrumbsLink } from "../parts";
 
 export const PageLayout = ({
-	className,
-	loading,
-	error,
-	left,
-	right,
-	categoryId,
-	product,
+  className,
+  loading,
+  error,
+  left,
+  right,
+  categoryId,
+  product,
 }: PageLayoutProps) => {
-	return (
-		<>
-			<CrumbsLink
-				isProductPage={true}
-				category={product?.params['Приналежність до категорії']}
-				title={product?.params.ModelName}
-				items={[]}
-				categories={{
-					id: categoryId,
-					category: { name: product?.params['Приналежність до категорії'] },
-				}}
-			/>
-			<div className={`flex justify-between ${className}`}>
-				{loading && <>Loading...</>}
-				{error && <>Error: {error}</>}
-				<div className='w-[905px] flex flex-col gap-6'>{left}</div>
-				<div className='w-[305px] flex flex-col gap-6'>{right}</div>
-			</div>
-		</>
-	)
-}
+  return (
+    <>
+      <CrumbsLink
+        isProductPage={true}
+        category={product?.params["Приналежність до категорії"]}
+        title={product?.params.ModelName}
+        items={[]}
+        categories={{
+          id: categoryId,
+          category: { name: product?.params["Приналежність до категорії"] },
+        }}
+      />
+      <div className={`flex justify-between ${className}`}>
+        {loading && <>Loading...</>}
+        {error && <>Error: {error}</>}
+        <div className="w-[905px] flex flex-col gap-6">{left}</div>
+        <div className="w-[305px] flex flex-col gap-6">{right}</div>
+      </div>
+    </>
+  );
+};
