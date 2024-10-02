5d4678f76c374a63899637ca4b793a4ab55abb3b
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-10-02 21:44:45 +0200

    Update CrumbsLink.tsx

--- components/parts/CrumbsLink.tsx
+++ components/parts/CrumbsLink.tsx
@@ -10,62 +10,35 @@ import {
 } from "../ui/breadcrumb";
 import { TCrumbs } from "@/types";
 
-const BreadcrumbHomeLink = () => (
-  <BreadcrumbItem>
-    <BreadcrumbLink asChild>
-      <Link href="/">Головна</Link>
-    </BreadcrumbLink>
-  </BreadcrumbItem>
-);
-
-const BreadcrumbCategoryLink = ({
-  categories,
-  isProductPage,
-}: {
-  categories: TCrumbs["categories"];
-  category: string | undefined;
-  isProductPage: boolean;
-}) => (
-  <BreadcrumbItem>
-    <BreadcrumbLink asChild>
-      <Link href={`/category/${categories?.id}`}>
-        {categories?.category.name}
-      </Link>
-    </BreadcrumbLink>
-  </BreadcrumbItem>
-);
-
-const BreadcrumbTitleLink = ({ title }: { title: string }) => (
-  <BreadcrumbPage>
-    <BreadcrumbItem>
-      <BreadcrumbLink asChild>
-        <span>{title}</span>
-      </BreadcrumbLink>
-    </BreadcrumbItem>
-  </BreadcrumbPage>
-);
-
-export const CrumbsLink = ({
-  categories,
-  items,
-  title,
-  category,
-  isProductPage,
-}: TCrumbs) => {
+export const CrumbsLink = ({ categories, title, isProductPage }: TCrumbs) => {
   return (
     <Breadcrumb className="py-7">
       <BreadcrumbList className="text-lg">
-        <BreadcrumbHomeLink />
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">Головна</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
         <BreadcrumbSeparator />
-        <BreadcrumbCategoryLink
-          category={category}
-          categories={categories}
-          isProductPage={isProductPage}
-        />
+        {categories && (
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href={`/category/${categories?.id}`}>
+                {categories?.category.name}
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        )}
         {isProductPage && title && (
           <>
             <BreadcrumbSeparator />
-            <BreadcrumbTitleLink title={title} />
+            <BreadcrumbPage>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <span>{title}</span>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbPage>
           </>
         )}
       </BreadcrumbList>
